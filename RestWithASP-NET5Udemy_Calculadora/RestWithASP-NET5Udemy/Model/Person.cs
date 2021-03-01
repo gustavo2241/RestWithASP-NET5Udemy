ff091218d1a7d18464c6f890b4ea8b32a37d04ed
gupascutti@uninove.edu.br <gupascutti@uninove.edu.br>
2021-03-01 12:06:00 -0300

    Implementando os 4 verbos

--- RestWithASP-NET5Udemy_Calculadora/RestWithASP-NET5Udemy/Model/Person.cs
+++ RestWithASP-NET5Udemy_Calculadora/RestWithASP-NET5Udemy/Model/Person.cs
@@ -9,7 +9,7 @@ namespace RestWithASP_NET5Udemy.Model
     {
         public long Id { get; set; }
         public string FirstName { get; set; }
-        public string lastName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
         public string Gender { get; set; }
     }
--- RestWithASP-NET5Udemy_Calculadora/RestWithASP-NET5Udemy/Startup.cs
+++ RestWithASP-NET5Udemy_Calculadora/RestWithASP-NET5Udemy/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestWithASP_NET5Udemy.Services.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,8 +26,9 @@ namespace RestWithASP_NET5Udemy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            //Injeção de dependencia
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
